fd01fceebec81f7745058d22e5f2f608e2ef49eb
javier162380 <javierllorente16@gmail.com>
2019-10-22 09:33:32 +0200

    adding the last test is not implemented yet

---
+++ tests/core/test_generate_schema.py
@@ -0,0 +1,8 @@
+import pytest
+import marshmallow
+
+def test_generate_marshmallow_schema(marshmayama_instance):
+    pass
+
+def test_generate_invalidad_marshmallow_schma(marshmayama_instance):
+    pass

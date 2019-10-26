a92cea25cdbf4462f43a1fab435d5e1fd6ca5c99
javier162380 <javierllorente16@gmail.com>
2019-10-26 07:41:39 +0200

    adding the last tests

--- tests/core/test_generate_schema.py
+++ tests/core/test_generate_schema.py
@@ -2,7 +2,82 @@ import pytest
 import marshmallow
 
 def test_generate_marshmallow_schema(marshmayama_instance):
-    pass
+    MarshInstance = marshmayama_instance('generic_schema.yml')
+    Marshschema = MarshInstance.generate_schema()
 
-def test_generate_invalidad_marshmallow_schma(marshmayama_instance):
-    pass
+    assert issubclass(type(Marshschema), type(marshmallow.Schema()))
+
+def test_generate_marshmallow_schema_valid_load(marshmayama_instance):
+    MarshInstance = marshmayama_instance('generic_schema.yml')
+    expected_results = {
+        "user_id":"Kayne",
+        "anonymous_id":"Mr West",
+        "metadata": {
+            "style": "rap"
+        }
+    }
+
+    MarshSchema = MarshInstance.generate_schema()
+    results = MarshSchema.load({
+        "userId": "Kayne",
+        "anonymousId": "Mr West",
+        "metadata": {
+            "style": "rap"
+        },
+        "notwantedevents": "randomness"
+    })
+
+    assert expected_results == results
+
+def test_generate_marshmallow_schema_invalid_load(marshmayama_instance):
+    MarshInstance = marshmayama_instance('generic_schema.yml')
+    MarshSchema = MarshInstance.generate_schema()
+
+    with pytest.raises(marshmallow.ValidationError):
+        MarshSchema.load({
+        "anonymousId": "Mr West",
+        "metadata": {
+            "style": "rap"
+        },
+        "notwantedevents": "randomness"
+    })
+
+def test_generate_marshmallow_schema_valid_dump(marshmayama_instance):
+    MarshInstance = marshmayama_instance('generic_schema.yml')
+    expected_results = {
+        "userId":"Kayne",
+        "anonymousId":"Mr West",
+        "metadata": {
+            "style": "rap"
+        }
+    }
+
+    MarshSchema = MarshInstance.generate_schema()
+    results = MarshSchema.dump({
+        "user_id": "Kayne",
+        "anonymous_id": "Mr West",
+        "metadata": {
+            "style": "rap"
+        }
+    })
+    assert expected_results == results
+
+def test_generate_marshmallow_schema_invalid_dump(marshmayama_instance):
+    MarshInstance = marshmayama_instance('generic_schema.yml')
+    expected_results = {
+        "user_id":"Kayne",
+        "anonymous_id":"Mr West",
+        "metadata": {
+            "style": "rap"
+        }
+    }
+
+    MarshSchema = MarshInstance.generate_schema()
+    results = MarshSchema.dump({
+        "user_id": "Kayne",
+        "anonymous_id": "Mr West",
+        "metadata": {
+            "style": "rap"
+        }
+    })
+    assert expected_results != results

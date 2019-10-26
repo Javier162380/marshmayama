a72db9aeecd9f30c1952f69f24054d343b22b0fa
javier162380 <javierllorente16@gmail.com>
2019-10-26 08:24:05 +0200

    adding setup correctly

--- setup.py
+++ setup.py
@@ -2,9 +2,15 @@ from setuptools import setup
 
 meta = {}
 exec(open('./marshmayama/version.py').read(), meta)
+libraries = open('requiremets.txt').read().split()
 
 setup(
     name="Marshmayama",
     description="Fancy library to generate marshmallow.schemas(), dinamically from a yaml file",
-    
+    version=meta['__version__'],
+    author="@Javier162380",
+    packages="marshmayama",
+    install_requires=libraries,
+    url="https://github.com/javier162380/marshmayama",
+    python_requires='>=3.6'
 )
